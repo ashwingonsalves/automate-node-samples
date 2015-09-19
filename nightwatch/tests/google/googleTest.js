3f2644632d09f3938804c40a557958aa900bf98a
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2015-09-19 13:07:54 +0530

    Update googleTest.js

--- nightwatch/tests/google/googleTest.js
+++ nightwatch/tests/google/googleTest.js
@@ -3,7 +3,7 @@ module.exports = {
     browser
       .url("http://www.google.com")
       .waitForElementVisible('body', 1000)
-      .setValue('input[type=text]', 'nightwatch')
+      .setValue('input[name=q]', 'nightwatch')
       .waitForElementVisible('button[name=btnG]', 1000)
       .click('button[name=btnG]')
       .pause(1000)
